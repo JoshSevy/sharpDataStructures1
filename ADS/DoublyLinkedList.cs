f84612777d7b858773ead3e6a0814cddacb98c23
JoshSevy <joshuasevy@outlook.com>
2022-08-23 22:53:49 -0600

    Setup C# data structures

---
+++ ADS/DoublyLinkedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace DataStructures
+{
+    /// <summary>
+    ///  A Node in the linked list
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class DoublyLinkedListNode<T>
+    {
+        /// <summary>
+        /// Constructs a new node with the specific value.
+        /// </summary>
+        /// <param name="value"></param>
+        public DoublyLinkedListNode(T value)
+        {
+            Value = value;
+        }
+        /// <summary>
+        /// The node value
+        /// </summary>
+        public T Value { get; set; }
+
+        /// <summary>
+        /// The next node in the linked list (null if last node)
+        /// </summary>
+        public DoublyLinkedListNode<T> Next { get; set; }
+
+        /// <summary>
+        /// The previous node in the linked list (null if first node)
+        /// </summary>
+        public DoublyLinkedListNode<T> Previous { get; set; }
+    }
+}
